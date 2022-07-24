f0a0988f9826cbaca99f80de8d184e3595f4b58a
함수원 <dev.code.su@gmail.com>
2022-07-24 14:14:12 +0900

    [MOD] sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
-const webpackPlugins = require('./webpack.plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: 'docs',
-  publicPath: '/stories/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/stories/' : '/',
   chainWebpack: config => {
     config.output.chunkFilename(`[id].[chunkhash:8].js`);
     config.plugins.delete('prefetch');
@@ -15,10 +14,5 @@ module.exports = defineConfig({
         maxSize: 4 * 1024,
       },
     });
-  },
-  configureWebpack: config => {
-    if (process.env.NODE_ENV === 'production') {
-      config.plugins.push(...webpackPlugins);
-    }
   },
 });
