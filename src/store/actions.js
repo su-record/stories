8c62d8d7ff8d581acc3d5be8f737988c6125672b
함수원 <dev.code.su@gmail.com>
2022-07-24 19:24:42 +0900

    [MOD] styles

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,13 +3,10 @@ import { getData } from '@/api';
 export default {
   async GET_POSTS({ commit }) {
     const { data } = await getData('posts/index.json');
-    console.log('### GET_POSTS ::', data);
     commit('SET_POSTS', data);
   },
   async GET_POST_CONTENT({ commit }, postPath) {
-    console.log('### GET_POST_CONTENT ::', postPath);
-    const { data } = await getData(`posts/${postPath}.md`);
-    console.log('### GET_POST_CONTENT ::', postPath);
+    const { data } = await getData(postPath);
     commit('SET_POST_CONTENT', data);
   },
 };
